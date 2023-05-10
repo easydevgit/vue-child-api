0eae0ca9ca9f20ab600a0c42027f782ba0d6c2e3
easydevgit <urserg1@mail.com>
2023-05-10 11:52:10 +0400

    slot props

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TimeMessage from '@/components/TimeMessage.vue';
+const currentDate = new Date();
+const eventDate = new Date(2024, 1, 1);
+
+</script>
+<template>
+  <div>
+    <TimeMessage
+        :eventDate="eventDate"
+        :currentDate="currentDate"
+        v-slot="{days, hours, minutes}"
+    >
+      {{ days }} days, {{ hours }} hours, {{ minutes }} minutes
+    </TimeMessage>
+  </div>
+</template>
---
+++ src/components/TimeMessage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  eventDate: Date,
+  currentDate: Date
+}>()
+
+const getCalcRemainingTime = () => {
+  const diff = props.eventDate.getTime() - props.currentDate.getTime()
+  const minutes = Math.floor((diff / 1000 / 60) % 60)
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24)
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+  return {
+    days,
+    hours,
+    minutes,
+  }
+}
+
+const calcRemainingTime = getCalcRemainingTime()
+
+</script>
+<template>
+  <slot v-bind="calcRemainingTime"/>
+</template>
